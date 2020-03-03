3ef2f5e2cf660f25b5b6b706a4dc258189d2be5c
Jodi N Kansagor <joedvice@gmail.com>
2020-03-02 17:04:38 -0800

    attempting to fix path name for travis

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Header } from '../components/common/Header';
+import { Header } from '../components/common/Header.js';
 
 export default function App() {
   return (
