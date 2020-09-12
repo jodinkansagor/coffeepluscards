020db4efccd2712a4696c71966c385f4db3a5b60
jodinkansagor <joedvice@gmail.com>
2020-09-12 16:27:39 -0700

    card actions tests passing

--- src/actions/cardActions.js
+++ src/actions/cardActions.js
@@ -1,11 +1,26 @@
-import { getCards } from "../services/cardApiServices";
+import { getCards, postCard } from "../services/cardApiServices";
 
 export const FETCH_GET_CARD_LOADING = "FETCH_GET_CARD_LOADING";
-export const FECTH_GET_CARDS = "FETCH_GET_CARDS";
+export const FETCH_POST_CARD_LOADING = "FETCH_POST_CARD_LOADING";
+export const FETCH_GET_CARDS = "FETCH_GET_CARDS";
+export const FETCH_POST_CARD = 'FETCH_POST_CARD'
+export const ADD_CARD = "ADD_CARD";
 
 export const fetchGetCards = () => (dispatch) => {
   dispatch({ type: FETCH_GET_CARD_LOADING });
   return getCards().then((cardArray) =>
-    dispatch({ type: FECTH_GET_CARDS, payload: cardArray })
+    dispatch({ type: FETCH_GET_CARDS, payload: cardArray })
   );
+};
+
+export const fetchPostCard = (card) => (dispatch) => {
+  dispatch({ type: FETCH_POST_CARD_LOADING });
+  return postCard(card).then((card) => dispatch(addCard(card)));
+};
+
+ export const addCard = (card) => {
+  return {
+    type: ADD_CARD,
+    payload: card,
+  };
 };
--- src/actions/cardActions.test.js
+++ src/actions/cardActions.test.js
@@ -1,10 +1,30 @@
 const {
   fetchGetCards,
   FETCH_GET_CARD_LOADING,
-  FECTH_GET_CARDS,
+  FETCH_GET_CARDS,
+  fetchPostCard,
+  FETCH_POST_CARD_LOADING,
+  FETCH_POST_CARD,
+  ADD_CARD
 } = require("./cardActions");
 
 jest.mock("../services/cardApiServices.js");
+
+const cardOne = {
+  date: "Feb 1",
+  card: "2 of cups",
+  deck: "Rider-Waite",
+  category: ["love", "family"],
+  explanation: "Mushy love stuff",
+};
+
+const cardTwo = {
+  date: "Feb 2",
+  card: "2 of swords",
+  deck: "Rider-Waite",
+  category: ["work", "family"],
+  explanation: "thinking a lot",
+};
 
 describe("get cards", () => {
   it("creates the get loading state", () => {
@@ -14,23 +34,23 @@ describe("get cards", () => {
     return action(dispatch).then(() => {
       expect(dispatch).toHaveBeenCalledWith({ type: FETCH_GET_CARD_LOADING });
       expect(dispatch).toHaveBeenCalledWith({
-        type: FECTH_GET_CARDS,
-        payload: [
-          {
-            date: "Feb 1",
-            card: "2 of cups",
-            deck: "Rider-Waite",
-            category: ["love", "family"],
-            explanation: "Mushy love stuff",
-          },
-          {
-            date: "Feb 2",
-            card: "2 of swords",
-            deck: "Rider-Waite",
-            category: ["work", "family"],
-            explanation: "thinking a lot",
-          },
-        ],
+        type: FETCH_GET_CARDS,
+        payload: [cardOne, cardTwo],
+      });
+    });
+  });
+});
+
+describe("post card", () => {
+  it("creates the card loading state for post", () => {
+    const dispatch = jest.fn();
+    const action = fetchPostCard(cardOne);
+
+    return action(dispatch).then(() => {
+      expect(dispatch).toHaveBeenCalledWith({ type: FETCH_POST_CARD_LOADING });
+      expect(dispatch).toHaveBeenCalledWith({
+        type: ADD_CARD,
+        payload: cardOne,
       });
     });
   });
